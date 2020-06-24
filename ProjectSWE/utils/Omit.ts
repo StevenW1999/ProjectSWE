50f1d1d75845eb584b22d634b7de37be95cba405
Beau <31695264+Beau@users.noreply.github.com>
2020-06-24 23:26:54 +0200

    Update Omit.ts

--- ProjectSWE/utils/Omit.ts
+++ ProjectSWE/utils/Omit.ts
@@ -1,3 +1,8 @@
+/**
+ * omit<T,K>
+ * A utility function used to construct a type by omitting K from T
+ */
+
 import * as Utils from './Fun';
 import * as Array from '../utils/ArrayFunctions';
 
@@ -7,7 +12,7 @@ export type omit<T, Conditions extends keyof T> = Pick<T, {
 
 export const omit = <T, K extends keyof T>(keys: Array<keyof T>): Utils.Fun<T, omit<T, K>> => (
     Utils.fun(object =>
-        Array.getKeysFromObject(object).map(key => (keys as Array<keyof T>).includes(key) ? {} : { [key]: object[key] })
+        Array.getKeysFromObject(object).map(key => (keys).some(k => k === key) ? {} : { [key]: object[key] })
         .reduce((res, o) => ({ ...res, ...o }), {}) as omit<T, K>
     )
 );
